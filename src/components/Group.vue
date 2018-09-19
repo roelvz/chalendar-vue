c6057c3fc7ec425e1a30e43f9f8d8b21c9fb3971
Roel Van Zeebroeck <rvz@dwesam.com>
2018-09-19 21:23:09 +0200

    Added dividers between group messages

--- src/components/Group.vue
+++ src/components/Group.vue
@@ -3,13 +3,15 @@
     <h1>{{loadedGroup.name}}</h1>
 
     <v-list>
+      <v-divider ></v-divider>
       <template v-for="message in loadedGroup.messages">
         <v-list-tile :key="message.id" @click="">
           <v-list-tile-content>
             <v-list-tile-title>{{message.text}}</v-list-tile-title>
-            <v-list-tile-sub-title>By RVZ</v-list-tile-sub-title>
+            <v-list-tile-sub-title>By {{message.creatorName}}</v-list-tile-sub-title>
           </v-list-tile-content>
         </v-list-tile>
+        <v-divider ></v-divider>
       </template>
     </v-list>
 
