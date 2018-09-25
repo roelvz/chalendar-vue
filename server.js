c27f94ccfbf65a1ba637e21ef5d4ea461d02f127
Roel Van Zeebroeck <rvz@dwesam.com>
2018-09-25 21:43:14 +0200

    Fix callback

--- server.js
+++ server.js
@@ -2,8 +2,21 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+
+// create the express app
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
+
+// create middleware to handle the serving the app
+app.use(serveStatic(path.join(__dirname, 'dist')));
+
+// Catch all routes and redirect to the index file
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/dist/index.html')
+})
+
+// Create default port to serve the app on
+const port = process.env.PORT || 5000;
 app.listen(port);
+
+// Log to feedback that this is actually running
 console.log('server started '+ port);
