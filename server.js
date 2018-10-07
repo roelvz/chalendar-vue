1564a719b48da7c09192ded71237d5313b6deb25
Roel Van Zeebroeck <rvz@dwesam.com>
2018-10-07 17:39:04 +0200

    Trying to fix notifications

--- server.js
+++ server.js
@@ -11,17 +11,21 @@ app.use(serveStatic(path.join(__dirname, 'dist')));
 
 // Check OneSignal SDK files and make sure they are not redirected to index file
 app.get("/manifest.json", (req, res) => {
+  console.log('manifest');
   res.sendFile(path.resolve(__dirname, "public", "./manifest.json"));
 });
 app.get("/OneSingalSDKWorker.js", (req, res) => {
+  console.log('OneSingalSDKWorker');
   res.sendFile(path.resolve(__dirname, "public", "./OneSingalSDKWorker.js"));
 });
 app.get("/OneSignalSDKUpdaterWorker.js", (req, res) => {
+  console.log('OneSignalSDKUpdaterWorker');
   res.sendFile(path.resolve(__dirname, "public", "./OneSignalSDKUpdaterWorker.js"));
 });
 // Catch all other routes and redirect to the index file
 app.get('*', function (req, res) {
-  console.log('XXXX');
+  console.log('*');
+  console.log(__dirname);
 
   res.sendFile(__dirname + '/dist/index.html')
 });
