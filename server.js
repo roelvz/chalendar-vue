cecbd65cfe32b562c120319738093b83e9dac56c
Roel Van Zeebroeck <rvz@dwesam.com>
2018-10-07 17:12:22 +0200

    Trying to fix notifications

--- server.js
+++ server.js
@@ -9,37 +9,15 @@ app = express();
 // create middleware to handle the serving the app
 app.use(serveStatic(path.join(__dirname, 'dist')));
 
-console.log('X');
-
-// Check OneSignal SDK files and make sure they are not redirected to index file
-app.get("manifest.json", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "/dist/manifest.json"));
-});
-app.get("OneSingalSDKWorker.js*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "/dist/OneSingalSDKWorker.js"));
-});
-app.get("OneSignalSDKUpdaterWorker.js", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "/dist/OneSignalSDKUpdaterWorker.js"));
-});
 // Check OneSignal SDK files and make sure they are not redirected to index file
 app.get("/manifest.json", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "/dist/manifest.json"));
+  res.sendFile(path.resolve(__dirname, "public", "./manifest.json"));
 });
-app.get("/OneSingalSDKWorker.js*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "/dist/OneSingalSDKWorker.js"));
+app.get("/OneSingalSDKWorker.js", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "./OneSingalSDKWorker.js"));
 });
 app.get("/OneSignalSDKUpdaterWorker.js", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "/dist/OneSignalSDKUpdaterWorker.js"));
-});
-// Check OneSignal SDK files and make sure they are not redirected to index file
-app.get("/dist/manifest.json", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "/dist/manifest.json"));
-});
-app.get("/dist/OneSingalSDKWorker.js*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "/dist/OneSingalSDKWorker.js"));
-});
-app.get("/dist/OneSignalSDKUpdaterWorker.js", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "/dist/OneSignalSDKUpdaterWorker.js"));
+  res.sendFile(path.resolve(__dirname, "public", "./OneSignalSDKUpdaterWorker.js"));
 });
 // Catch all other routes and redirect to the index file
 app.get('*', function (req, res) {
