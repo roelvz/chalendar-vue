3fb4f1706ba85e325fba8355a945abfc7abdef75
Roel Van Zeebroeck <rvz@dwesam.com>
2018-10-07 18:11:20 +0200

    Trying to fix notifications

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,5 +3,5 @@ module.exports = {
   NODE_ENV: '"production"',
   BACKEND_API_URI: '"https://chalendar.herokuapp.com/api/"',
   LOGIN_CALLBACK_URI: '"https://chalendar-ui.herokuapp.com/callback"',
-  ONESIGNAL_APP_ID: '"6c6dda35-89d1-4314-8ce8-59d8f72876df"',
+  ONESIGNAL_APP_ID: '"8fea93ea-1eb7-47d5-ba69-414b7d343a78"',
 }
