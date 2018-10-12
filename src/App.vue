fbce5f38d88c8aec8fafb64eca5dda5ebd45c8a5
Roel Van Zeebroeck <rvz@dwesam.com>
2018-10-12 17:51:54 +0200

    Name logging

--- src/App.vue
+++ src/App.vue
@@ -153,6 +153,7 @@ export default {
         OneSignal.push(function() {
           OneSignal.sendTag("chalendar_user", temp.userInfo.sub);
 
+          // TODO: refactor
           let given_name = temp.userInfo.given_name;
           if (!temp.userInfo.given_name) {
             if (temp.userInfo.family_name) {
@@ -163,6 +164,8 @@ export default {
               given_name = 'Unknown';
             }
           }
+          console.log('given name: ' + given_name);
+          console.log(temp.userInfo);
           if (given_name) {
             OneSignal.sendTag("chalendar_username", given_name);
           }
