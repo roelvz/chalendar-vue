d58dd050e5e46f224ea218c6077fe8e3cf3711e4
Roel Van Zeebroeck <rvz@dwesam.com>
2018-10-23 22:53:57 +0200

    Revert "Check session before authorization"
    This reverts commit a0a1a1d

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -21,20 +21,11 @@ var auth = new auth0.WebAuth({
 });
 
 export function login() {
-  console.log("checkSession");
-  auth.checkSession({
+  auth.authorize({
     responseType: 'token id_token',
     redirectUri: REDIRECT,
     audience: AUDIENCE,
     scope: SCOPE
-  }, function(err, authResult) {
-    console.log("checkSession failed: authorize");
-    auth.authorize({
-      responseType: 'token id_token',
-      redirectUri: REDIRECT,
-      audience: AUDIENCE,
-      scope: SCOPE
-    });
   });
 }
 
