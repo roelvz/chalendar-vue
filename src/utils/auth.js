84a7764503b37f344e64f3d094c08c800c924904
Roel Van Zeebroeck <rvz@dwesam.com>
2018-10-24 18:51:21 +0200

    Removed checkSession for now

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -21,21 +21,21 @@ var auth = new auth0.WebAuth({
 });
 
 export function login() {
-  console.log("checkSession");
-  auth.checkSession({
-    responseType: 'token id_token',
-    redirectUri: REDIRECT,
-    audience: AUDIENCE,
-    scope: SCOPE
-  }, function(err, authResult) {
-    console.log("checkSession failed: authorize");
+  // console.log("checkSession");
+  // auth.checkSession({
+  //   responseType: 'token id_token',
+  //   redirectUri: REDIRECT,
+  //   audience: AUDIENCE,
+  //   scope: SCOPE
+  // }, function(err, authResult) {
+  //   console.log("checkSession failed: authorize");
     auth.authorize({
       responseType: 'token id_token',
       redirectUri: REDIRECT,
       audience: AUDIENCE,
       scope: SCOPE
     });
-  });
+  // });
 }
 
 var router = new Router({
