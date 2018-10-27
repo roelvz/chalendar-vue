fe99ffc0837729d4023811aa8243a7d1f525edef
Roel Van Zeebroeck <rvz@dwesam.com>
2018-10-27 18:45:58 +0200

    Fix callback URI for development

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   BACKEND_API_URI: '"http://localhost:3000/api/"',
-  LOGIN_CALLBACK_URI: '"http://localhost:3000/callback"',
+  LOGIN_CALLBACK_URI: '"http://localhost:8080/callback"',
 })
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -9,7 +9,7 @@ const ACCESS_TOKEN_KEY = 'access_token';
 
 const CLIENT_ID = '26K0G3dpKG3JVSMgDUCM2c4OUoxsDZY1';
 const CLIENT_DOMAIN = 'chalendar.eu.auth0.com';
-const REDIRECT = `https://challendar-ui.herokuapp.com/callback`;
+const REDIRECT = process.env.LOGIN_CALLBACK_URI;
 const SCOPE = 'openid profile email';
 const AUDIENCE = 'https://chalendar.com/';
 
