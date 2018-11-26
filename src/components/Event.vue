58e759787fc680cb8437b0accfcc902af60521cc
Maarten De Brandt <maartendb@gmail.com>
2018-11-26 17:16:09 +0100

    Description added to Events screen

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -9,7 +9,8 @@
       
         <h1> {{loadedEvent.name}}  </h1>
         <h2>{{new Date(loadedEvent.date).toDateString()}}</h2>
-        <h5>{{addedInformationString()}}</h5>         
+        <h5>{{addedInformationString()}}</h5>     
+        <h3>{{loadedEvent.description}}</h3>    
         <v-radio-group v-model="attendance">
           <!--TODO: constants-->
           <v-radio :label="goingString()" value="going"></v-radio>
